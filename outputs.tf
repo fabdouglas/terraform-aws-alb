16b0f6e4a5cb75c020446c21fe9a7f0076703c99
Fabrice Daugan <fabrice.daugan@gmail.com>
2018-04-23 16:29:39 +0200

    Optional bucket access logs

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "dns_name" {
   description = "The DNS name of the load balancer."
-  value       = "${aws_lb.application.dns_name}"
+  value       = "${join(",",aws_lb.application.dns_name)}"
 }
 
 output "http_tcp_listener_arns" {
@@ -25,17 +25,17 @@ output "https_listener_ids" {
 
 output "load_balancer_arn_suffix" {
   description = "ARN suffix of our load balancer - can be used with CloudWatch."
-  value       = "${aws_lb.application.arn_suffix}"
+  value       = "${join(",",aws_lb.application.arn_suffix)}"
 }
 
 output "load_balancer_id" {
   description = "The ID and ARN of the load balancer we created."
-  value       = "${aws_lb.application.id}"
+  value       = "${join(",",aws_lb.application.id)}"
 }
 
 output "load_balancer_zone_id" {
   description = "The zone_id of the load balancer to assist with creating DNS records."
-  value       = "${aws_lb.application.zone_id}"
+  value       = "${join(",",aws_lb.application.zone_id)}"
 }
 
 output "target_group_arns" {
